e6e3db6e86654cfde95a54df0015862b5a8046dc
JHui29 <43899410+JHui29@users.noreply.github.com>
2019-02-11 11:36:09 +0800

    Update

--- 20190203_OOP_Assignment/20190203_OOP_Java_Assignement/src/control/Admin.java
+++ 20190203_OOP_Assignment/20190203_OOP_Java_Assignement/src/control/Admin.java
@@ -25,32 +25,35 @@ public class Admin {
 	    }
 	}
 	
-	public void displayAdminMenu () {
+	public void displayAdminMenu() 
+	{
 		clearConsole();
 		System.out.println("******************** Admin Menu *********************");
 		System.out.println("* 1. Employee Record								*");
 		System.out.println("* 2. Claim Type Record								*");
-		System.out.println("* 2. Claim Record									*");
-		System.out.println("* 3. Back To Main Menu                            	*");
+		System.out.println("* 3. Claim Record									*");
+		System.out.println("* 4. Back To Main Menu                            	*");
 		System.out.println("* Enter the number and press <enter> to continue	*");
 		System.out.println("*****************************************************");	
 	}
 	
 	
-	public void displayEmployeeMenu(){	
+	public void displayEmployeeMenu()
+	{	
 		clearConsole();
 		System.out.println("******************* Employee Menu *******************");
 		System.out.println("* 1. Add Employee									*");
 		System.out.println("* 2. Edit Employee									*");
 		System.out.println("* 3. Delete Employee								*");
-		System.out.println("* 3. View Employee									*");
-		System.out.println("* 3. Search Employee								*");
-		System.out.println("* 4. Back to Admin Menu								*");
+		System.out.println("* 4. View Employee									*");
+		System.out.println("* 5. Search Employee								*");
+		System.out.println("* 6. Back to Admin Menu								*");
 		System.out.println("* Enter the number and press <enter> to continue	*");
 		System.out.println("*****************************************************");		
 	}
 	
-	public void displayClaimMenu(){
+	public void displayClaimMenu()
+	{
 		clearConsole();
 		System.out.println("******************* Claim Menu **********************");
 		System.out.println("* 1. Add Claim										*");
@@ -64,7 +67,8 @@ public class Admin {
 		
 	}
 	
-	public void displayClaimTypeMenu(){
+	public void displayClaimTypeMenu()
+	{
 		clearConsole();
 		System.out.println("****************** Claim Type Menu ******************");
 		System.out.println("* 1. Add Claim Type									*");
@@ -77,11 +81,161 @@ public class Admin {
 		System.out.println("*****************************************************");
 	}
 	
-	public void addEmployee() {
+	public void adminMenuView()
+	{
+		Scanner input = new Scanner(System.in);
+		displayAdminMenu();
+		
+		int choice = input.nextInt(); 
+		
+		switch(choice)
+		{
+			case 1: adminEmployeeView();
+			case 2: adminClaimTypeView();
+			case 3: adminClaimView();
+			case 4: ;
+		}
+		input.close();
+	}
+	
+	public void adminEmployeeView()
+	{
+		Scanner input = new Scanner(System.in);
+		displayEmployeeMenu();
+		
+		int choice = input.nextInt(); 
+		
+		switch(choice)
+		{
+			case 1: addEmployee();
+			case 2: editEmployee();
+			case 3: deleteEmployee();
+			case 4: viewEmployee();
+			case 5: searchEmployee();
+			case 6: adminMenuView();
+		}
+		input.close();
+	}
+	
+	public void adminClaimTypeView()
+	{
+		Scanner input = new Scanner(System.in);
+		displayClaimTypeMenu();
+		
+		int choice = input.nextInt(); 
+		
+		switch(choice)
+		{
+			case 1: addClaimType();
+			case 2: editClaimType();
+			case 3: deleteClaimType();
+			case 4: viewClaimType();
+			case 5: searchClaimType();
+			case 6: adminMenuView();
+		}
+		input.close();
+	}
+	
+	public void adminClaimView()
+	{
+		Scanner input = new Scanner(System.in);
+		displayClaimMenu();
+		
+		int choice = input.nextInt(); 
+		
+		switch(choice)
+		{
+			case 1: addClaim();
+			case 2: editClaim();
+			case 3: deleteClaim();
+			case 4: viewClaim();
+			case 5: searchClaim();
+			case 6: adminMenuView();
+		}
+		input.close();
+	}
+	
+	// Employee
+	
+	public void addEmployee()
+	{
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Enter employee id :");
 		String empID = input.nextLine();
 	}
 	
+	public void editEmployee()
+	{
+		
+	}
+	
+	public void deleteEmployee()
+	{
+		
+	}
+	
+	public void viewEmployee()
+	{
+		
+	}
+	
+	public void searchEmployee()
+	{
+		
+	}
+
+	// Claim Type
+	
+	public void addClaimType()
+	{
+		
+	}
+	
+	public void editClaimType()
+	{
+		
+	}
+	
+	public void deleteClaimType()
+	{
+		
+	}
+	
+	public void viewClaimType()
+	{
+		
+	}
+	
+	public void searchClaimType()
+	{
+		
+	}
+	
+	// Claim
+	
+	public void addClaim()
+	{
+		
+	}
+	
+	public void editClaim()
+	{
+		
+	}
+	
+	public void deleteClaim()
+	{
+		
+	}
+	
+	public void viewClaim()
+	{
+		
+	}
+	
+	public void searchClaim()
+	{
+		
+	}
 }
